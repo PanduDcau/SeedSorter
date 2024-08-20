c55f06fbdc902f1dd36ea0c286b5b5a2b266afbf
W. Pandula <52339029+PanduDcau@users.noreply.github.com>
2024-08-21 00:38:36 +0530

    work

--- pepsorter/app.py
+++ pepsorter/app.py
@@ -1,5 +1,5 @@
-## Importing flask
 from flask import Flask
+import os
 
 # Define a flask app
 app = Flask(__name__)
@@ -9,7 +9,10 @@ RESULTS_FOLDER = 'results'
 app.secret_key = "secret key"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['RESULTS_FOLDER'] = RESULTS_FOLDER
+app.config['OUTPUT_FOLDER'] = 'output'
 
-import os
+
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 os.makedirs(RESULTS_FOLDER, exist_ok=True)
+os.makedirs('results_process', exist_ok=True)
+os.makedirs('output', exist_ok=True)
